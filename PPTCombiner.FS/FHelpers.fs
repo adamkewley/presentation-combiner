ef91dd837aceb1a4060f64f057021c5d64a8ee32
Adam <adamk117@gmail.com>
2014-06-23 21:05:33 +0100

    Unstable: Changed F# backend. Need to hook it up to C# frontend. Application has moved from using path strings all over to using a new type (PPTCombiner.FS.AddedPath) which holds more information. This will eventually be hooked into the UI so that more info than just the added paths is shown

--- PPTCombiner.FS/FHelpers.fs
+++ PPTCombiner.FS/FHelpers.fs
@@ -5,52 +5,20 @@ open System
 open System.IO
 open System.Reactive.Subjects
 
-let validExtensions = [ "*.ppt"; "*.pptx" ]
-
-// Returns a sequence of valid files found within a directory.
-let GetValidFilesInDirectory (directoryPath : string) : string seq =
-
-    let getValidFiles validExtension = 
-        Directory.GetFiles(directoryPath, validExtension, SearchOption.TopDirectoryOnly)
-
-    Seq.map getValidFiles validExtensions
-    |> Seq.concat
-    |> Seq.map (fun file -> FileInfo(file).FullName)
-    |> Seq.distinct
-
-// Returns true if the path provided is a file with a valid merge extension.
-let IsAValidFile (filePath : string) : bool =
-
-    let fileInfo = FileInfo(filePath)
-
-    Seq.filter (fun validExtension -> fileInfo.Extension = validExtension) validExtensions
-    |> Seq.isEmpty
-    |> not
-
-// Find valid file(s) for merging at a path. Can be a directory or a filepath.
-let FindValidFilesInPath (path : string) : string seq = seq {
-    if Directory.Exists path then
-        yield! GetValidFilesInDirectory path
-    else if File.Exists path && IsAValidFile path then
-        yield FileInfo(path).FullName
-    else
-        ArgumentOutOfRangeException("A string that isn't a file or a directory was passed in.")
-        |> raise
-}
     
 // Produces dynamic helper text that indicates the total number of slides found.
-let PathListToButtonText (pathList : ObservableCollection<string>) : BehaviorSubject<string> = 
+let PathListToButtonText (addedFilesList : ObservableCollection<AddedPath>) : BehaviorSubject<string> = 
 
-    let countTotalValidFiles lst =
-        Seq.map FindValidFilesInPath lst
-        |> Seq.concat
-        |> Seq.length
+    let countTotalValidFiles =
+        Seq.filter(fun addedPath -> addedPath.PathType <> PathType.InvalidFile)
+        >> Seq.map(fun addedPath -> addedPath.FileCount)
+        >> Seq.fold (+) 0
 
     let subject = new BehaviorSubject<string>("Nothing to merge.")
 
-    pathList.CollectionChanged 
+    addedFilesList.CollectionChanged 
     |> Observable.map (fun _ ->
-            match countTotalValidFiles pathList with
+            match countTotalValidFiles addedFilesList with
             | 0 -> "Nothing to merge."
             | 1 -> "Open one file."
             | x -> "Merge " + x.ToString() + " files.")
---
+++ PPTCombiner.FS/Paths.fs
@@ -0,0 +1,78 @@
+﻿namespace PPTCombiner.FS
+
+type PathType = ValidFile | InvalidFile | Folder| EmptyFolder| InvalidPath
+
+type AddedPath = { 
+    AddedPath : string
+    PathType : PathType
+    FileCount : int 
+    ContainedPaths : AddedPath seq option }
+
+module PathHelpers =
+    open System.Collections.ObjectModel
+    open System
+    open System.IO
+    open System.Reactive.Subjects
+
+    let ValidExtensions = 
+        [ "*.ppt"
+          "*.pptx" ]
+
+    // Returns a sequence of valid files found within a directory.
+    let GetValidFilesInDirectory (directoryPath : string) : string seq =
+
+        let getValidFiles validExtension = 
+            Directory.GetFiles(directoryPath, validExtension, SearchOption.TopDirectoryOnly)
+
+        Seq.map getValidFiles ValidExtensions
+        |> Seq.concat
+        |> Seq.map (fun file -> FileInfo(file).FullName)
+        |> Seq.distinct
+
+    // Returns true if the path provided is a file with a valid merge extension.
+    let IsAValidFile (filePath : string) : bool =
+
+        let fileInfo = FileInfo(filePath)
+
+        Seq.filter (fun validExtension -> fileInfo.Extension = validExtension) ValidExtensions
+        |> Seq.isEmpty |> not
+
+    // Find valid file(s) for merging at a path. Can be a directory or a filepath.
+    let rec FindValidFilesInPath (path : string) : AddedPath =
+        if Directory.Exists path then
+            // It's a directory (could contain no files though).
+
+            let validFiles = GetValidFilesInDirectory path
+            
+            if validFiles = Seq.empty then 
+                // A directory containing no presentation files.
+                { AddedPath = path
+                  PathType = EmptyFolder
+                  FileCount = 0
+                  ContainedPaths = None }
+            else
+                // A directory containing at least one valid file.
+                { AddedPath = path
+                  PathType = Folder
+                  FileCount = Seq.length validFiles
+                  ContainedPaths = Some(Seq.map FindValidFilesInPath validFiles) }
+
+        else if File.Exists path && IsAValidFile path then
+            if IsAValidFile path then
+                // A valid file.
+                { AddedPath = path
+                  PathType = ValidFile
+                  FileCount = 1
+                  ContainedPaths = None }
+            else
+                // An invalid file.
+                { AddedPath = path
+                  PathType = InvalidFile
+                  FileCount = 1
+                  ContainedPaths = None }
+        else
+            // An invalid path was added.
+            { AddedPath = path
+              PathType = InvalidFile
+              FileCount = 0
+              ContainedPaths = None }
