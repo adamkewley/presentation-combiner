30dc57569cd7d22683875eaf4e6eb9e79fb6d1b1
Adam <adamk117@gmail.com>
2014-06-28 15:26:19 +0100

    Implemented a two-way observablecollection for UI mapping, about to implement into UI.

--- PPTCombiner.FS/Lib.fs
+++ PPTCombiner.FS/Lib.fs
@@ -7,6 +7,111 @@ open System.Collections.ObjectModel
 // Copied from http://fssnip.net/dv
 module ObservableCollection =
 
+    (*
+    A two way observablecollection is quite difficult to implement.
+    We have two ObservableCollections, A<X> and B<Y>, and two mappers: f (X -> Y) and g (Y -> X).
+    We can map elements in A<X> to B<Y> with f: f(x1) -> y1
+    We can map elements in B<X> to A<Y> with g: f(y1) -> x1
+
+    Desired Behaviors:
+    - Any changes in both A<X> and B<Y> will be echoed via a CollectionChanged event for UI handling.
+        - There are no 'invisible' changes that the UI might miss.
+    - Any changes in A<X> will be mapped onto B<Y>.
+        - e.g. adding element x1 to A<X> will result in y1 being added to B<Y>.
+    - Any changes in B will be mapped onto A.
+        - e.g. adding element y1 to B<Y> will result in x1 being added to A<X>.
+    - Mapped changes will not be circular.
+        - e.g. after adding x1 to A<X>:
+            Intended:
+                - y1 added to B<Y> via f(x1) through the mapping.
+                - End
+            Unintended:
+                - y1 added to B<Y> via f(x1) through the mapping.
+                - THEN: x1 added (again) to A<X> via g(f(x1)) through mapping.
+                - THEN: y1 added (again) to B<Y> via f(g(f(x1))) through mapping.
+                - Crashes (StackOverflow, thread gets eaten up traversing callbacks).
+
+    Implementation Barriers:
+    - During a 'real' change the mapping is active.
+    - For mapping based changes, mapping is inactive.
+
+    Solutions:
+
+    --- A subscription (the thing doing the mapping) will disable its opposite subscription during changes.
+        Issues:
+            - Circular references required e.g.
+                - A<X> needs access to B<Y> subscription to create its subscription so it may
+                  disable it while it performs its mapping.
+                - B<Y> needs access to A<X> subscription to create its subscription so it may
+                  disable it while it performs its mapping.
+                - Cant create either subscription without the other one already existing
+        Workaround:
+            - Mutable reference cell that points to the subscription.
+                - A<X> uses the mutable subscription ref cell to B<Y>'s (not yet existient) subscription
+                - Likewise for B<Y>.
+                - After both subscriptions are established, the reference cells are occupied by them.
+
+    --- Override standard ObservableCollection and manually sort out mappings (e.g. not via a subscription).
+        Issues: 
+            - Multiple methods must be overrriden (Add, Remove, RemoveAt, etc.)
+            - Function signature must not accept a sourceOc with standard OC implementation. Both returned OCs are the derived type.
+                e.g.
+                    Original Signature:     oc1.TwoWayDynamicMap(f, g) -> ObservableCollection<Y>
+                    New Signature:          CreatePairedCollections(f, g) -> ObservableCollection<X> * ObservableCollection<Y>
+    
+    *)
+
+    let TwoWayDynamicMap (f : 'a -> 'b) (g : 'b -> 'a) (sourceOc : ObservableCollection<'a>) : ObservableCollection<'b> * IDisposable =
+
+        let destinationOc = ObservableCollection<_>()
+
+        // A handler for NotifyCollectionChangedEvent that will echo changes between oc1 and oc2 via a mapper
+        let mirrorTo (oc2 : ObservableCollection<_>) mapper (isLocked : bool ref) (changes : NotifyCollectionChangedEventArgs) =
+            if not (isLocked.Value) then
+                match changes.Action with
+                    | NotifyCollectionChangedAction.Add ->
+                        changes.NewItems |> Seq.cast<_> |> Seq.iteri (fun index item ->
+                            oc2.Insert(changes.NewStartingIndex + index, mapper item))
+
+                    | NotifyCollectionChangedAction.Move ->
+                        oc2.Move(changes.OldStartingIndex, changes.NewStartingIndex)
+
+                    | NotifyCollectionChangedAction.Remove ->
+                        oc2.RemoveAt(changes.OldStartingIndex)
+
+                    | NotifyCollectionChangedAction.Replace -> 
+                        changes.NewItems |> Seq.cast<_> |> Seq.iteri (fun index item ->
+                            oc2.[changes.NewStartingIndex + index] <- mapper item)
+
+                    | NotifyCollectionChangedAction.Reset -> oc2.Clear()
+
+                    | _ -> failwith "Unexpected action!"
+
+        // Create mutable reference cells that hold the locked status. 
+        // If locked (e.g. xLocked = true), "mirrorTo x f xLocked yChanges" wont echo yChanges onto x.
+        let sourceLocked = ref(false)
+        let destinationLocked = ref(false)
+
+        // Subscribe to source. Lock the destination from using the handler below to circularly map.
+        let sourceSub = sourceOc.CollectionChanged.Subscribe(fun change ->
+            do destinationLocked := true
+            do mirrorTo destinationOc f sourceLocked change
+            do destinationLocked := false)
+
+        // Subscribe to destination. Lock the source from using the handler above to circularly map.
+        let destinationSub = destinationOc.CollectionChanged.Subscribe(fun change ->
+            do sourceLocked := true
+            do mirrorTo sourceOc g destinationLocked change
+            do sourceLocked := false)
+
+        let removeLink = 
+            { new IDisposable with
+                member this.Dispose() = 
+                    sourceSub.Dispose()
+                    destinationSub.Dispose() }
+
+        (destinationOc, removeLink)
+
     /// <summary>
     /// Create a derived ObservableCollection which mirrors changes in the source collection.
     /// </summary>
@@ -47,6 +152,11 @@ module OcExtensions =
     /// <param name="f">The mapping function.</param>
     [<System.Runtime.CompilerServices.Extension>]
     let DynamicMap(sourceObservableCollection, f) = ObservableCollection.map (FSharpFunc.FromConverter(f)) sourceObservableCollection
+
+    [<System.Runtime.CompilerServices.Extension>]
+    let TwoWayDynamicMap(sourceOc, f, g) = 
+        let oc, dispose = ObservableCollection.TwoWayDynamicMap (FSharpFunc.FromConverter(f)) (FSharpFunc.FromConverter(g)) sourceOc
+        Tuple<_,_>(oc, dispose)
 
 
 // Simplified `guard` from http://stackoverflow.com/questions/3845110/observable-from-sequence-in-f
